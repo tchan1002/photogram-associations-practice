3f292dda81a1931ef47b528d8592813eb9439dc5
tchan1002 <tchan0@uchicago.edu>
2024-11-15 04:38:11 +0000

    triumph

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -15,10 +15,10 @@ class FollowRequest < ApplicationRecord
   validates(:recipient, { :presence => true })
   validates(:recipient_id, {
     :uniqueness => { :scope => [:sender_id] }
+  })
 
   belongs_to(:sender, class_name:"User", foreign_key:"sender_id")
   belongs_to(:recipient, class_name:"User", foreign_key:"recipient_id")
-  })
 
   # Association accessor methods to define:
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -52,8 +52,8 @@ class User < ApplicationRecord
   # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
   has_many(:accepted_received_follow_requests, -> { where status: "accepted" }, class_name: "FollowRequest", foreign_key: :recipient_id)
 
-  has_many(:liked_photos, through: :likes, source: :photos)
-  has_many(:commented_photos, through: :comments, source: :photos)
+  has_many(:liked_photos, through: :likes, source: :photo)
+  has_many(:commented_photos, through: :comments, source: :photo)
 
   ## Indirect associations (still need to do)
 
